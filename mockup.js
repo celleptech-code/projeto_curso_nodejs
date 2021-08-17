90a55df7dcd8377eb5f962db0f8f0d2e9124cf42
Luiz Gustavo <lgsouza.sp@gmail.com>
2021-08-17 09:14:15 -0300

    mockup update

--- mockup.js
+++ mockup.js
@@ -2,43 +2,43 @@ module.exports = [
     {
         id_noticia: 0,
         titulo: 'Blue Origin completa primeiro voo tripulado com Jeff Bezos',
-        noticia: 'O voo suborbital foi realizado nesta terça-feira e levou os primeiros passageiros da Blue Origin ao espaço. Veja como foi a viagem.',
+        conteudo: 'O voo suborbital foi realizado nesta terça-feira e levou os primeiros passageiros da Blue Origin ao espaço. Veja como foi a viagem.',
         data_criacao: '2021-08-22T09:00:00.000Z'
     },
     {
         id_noticia: 1,
         titulo: 'iOS 14.7 é lançado com correções e melhorias de segurança',
-        noticia: 'Além disso, a Apple também liberou atualizações para Apple TV e WatchOS.',
+        conteudo: 'Além disso, a Apple também liberou atualizações para Apple TV e WatchOS.',
         data_criacao: '2021-08-22T09:30:00.000Z'
     },
     {
         id_noticia: 2,
         titulo: 'Galaxy Book Pro e Pro 360 chegam ao Brasil partir de R$ 10,1 mil',
-        noticia: 'Enquanto o Galaxy Book Pro está custando R$ 11,5 mil, o Galaxy Book Pro 360 sai por R$ 10,1 mil.',
+        conteudo: 'Enquanto o Galaxy Book Pro está custando R$ 11,5 mil, o Galaxy Book Pro 360 sai por R$ 10,1 mil.',
         data_criacao: '2021-08-21T10:00:00.000Z'
     },
     {
         id_noticia: 3,
         titulo: 'Ex-funcionários do WhatsApp criam o HalloApp, uma rede social "real"',
-        noticia: 'A autodeclarada "primeira rede de relacionamentos reais" já está disponível na App Store da Apple e no Google Play.',
+        conteudo: 'A autodeclarada "primeira rede de relacionamentos reais" já está disponível na App Store da Apple e no Google Play.',
         data_criacao: '2021-08-21T10:30:00.000Z'
     },
     {
         id_noticia: 4,
         titulo: 'PES agora se chama eFootball, muda de engine e será gratuito',
-        noticia: 'O game será lançado para PS5, PS4, Xbox Series X/S, Xbox One, PC e mobile, com crossplay e usará a Unreal Engine 4.',
+        conteudo: 'O game será lançado para PS5, PS4, Xbox Series X/S, Xbox One, PC e mobile, com crossplay e usará a Unreal Engine 4.',
         data_criacao: '2021-08-20T11:00:00.000Z'
     },
     {
         id_noticia: 5,
         titulo: 'Campus Party 2021 Retorna em formato digital',
-        noticia: 'm 2021, a edição da América Latina vai ocorrer em seis países: Colômbia, Chile, Uruguai, Paraguai, Argentina e Brasil. Com apoio de governos locais, a edição brasileira terá três estúdios localizados em Brasília, Goiânia e São Paulo.',
+        conteudo: 'Em 2021, a edição da América Latina vai ocorrer em seis países: Colômbia, Chile, Uruguai, Paraguai, Argentina e Brasil. Com apoio de governos locais, a edição brasileira terá três estúdios localizados em Brasília, Goiânia e São Paulo.',
         data_criacao: '2021-08-20T16:30:00.000Z'
     },
     {
         id_noticia: 6,
         titulo: 'O que é smart home e como tornar a sua casa automatizada?',
-        noticia: 'Por não ser tão comum entre o grande público, o termo smart home (ou "casa inteligente", em tradução livre) não é muito conhecido e, às vezes, pode parecer uma realidade bem distante daquilo que podemos ter em nossas casas. No entanto, saiba que, se você achava que só poderia ver uma smart home em filmes, a realidade da automação residencial está mais próxima do que se imagina.',
+        conteudo: 'Por não ser tão comum entre o grande público, o termo smart home (ou "casa inteligente", em tradução livre) não é muito conhecido e, às vezes, pode parecer uma realidade bem distante daquilo que podemos ter em nossas casas. No entanto, saiba que, se você achava que só poderia ver uma smart home em filmes, a realidade da automação residencial está mais próxima do que se imagina.',
         data_criacao: '2021-08-19T18:30:00.000Z'
     },
 ]
